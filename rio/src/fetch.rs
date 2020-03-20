9fa3917648bb71001080fc9882a5e412559b24f3
Raphael Amorim <rapha850@gmail.com>
2020-03-19 21:36:25 -0300

    update

--- rio/src/fetch.rs
+++ rio/src/fetch.rs
@@ -1,5 +1,10 @@
-// use failure::{bail, format_err, Fallible};
+use std::fs;
+use std::io::{BufRead, BufReader, BufWriter, Seek, SeekFrom, Write};
+use std::path::Path;
+
 use failure::{Fallible};
+
+//http://localhost:3000/package/github.com.expressjs.express-4.17.1.tar.gz
 
 pub fn fetch_packages() -> Fallible<()> {
     Ok(())
