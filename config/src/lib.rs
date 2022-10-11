04735a962a897ecab9cc024528ec047469732cc6
1garo <alevardai427@gmail.com>
2022-10-11 12:49:11 -0300

    initial idea on how to read toml file

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,5 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, PartialEq)]
+enum Performances {
+    High,
+    Average,
+    Low,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Config {
+    performance: Performances,
+    width: Option<u16>,
+    height: Option<u16>,
+}
+
+pub fn load() -> Config {
+    let toml_str = r#"
+    # Rio configuration file
+
+    # <perfomance> Set WGPU rendering perfomance
+    # default: High
+    # options: High, Average, Low
+    performance = "High"
+
+    # <height> Set default height
+    # default: 400
+    height = 400
+
+    # <width> Set default width
+    # default: 600
+    width = 600
+
+    ## TODO: Add more configs
+    "#;
+
+    let decoded: Config = toml::from_str(toml_str).unwrap();
+
+    decoded
 }
 
 #[cfg(test)]
@@ -7,8 +44,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn load_config() {
+        let expected = Config {
+            performance: Performances::High,
+            width: Some(600),
+            height: Some(400),
+        };
+
+        let result = load();
+        assert_eq!(result, expected);
     }
 }
