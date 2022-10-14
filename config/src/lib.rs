7e46b2752e1cd5f6855011c84954dccabeb97c29
1garo <alevardai427@gmail.com>
2022-10-14 19:19:07 -0300

    load from path to improve test implementation

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 use std::default::Default;
+use std::io::Write;
 
 #[derive(Default, Debug, Deserialize, PartialEq, Clone, Copy)]
 enum Performance {
@@ -25,19 +26,72 @@ impl Config {
         }
     }
 
-    fn load() -> Self {
-        if std::path::Path::new("/Users/user/.rio/config.toml").exists() {
-            let content = std::fs::read_to_string("/Users/user/.rio/config.toml").unwrap();
-            // TODO: how can we parse a optional field and set a default for it after?
-            // if we let if be Option<T> we will have a None
-            // e.g when performance is Option<T>: { inner: ErrorInner  message: "missing field `performance`", key: [] } }
-            let decoded: Config = toml::from_str(&content).unwrap();
+    fn load_from_path(path: &str) -> Self {
+        if std::path::Path::new(path).exists() {
+            let content = std::fs::read_to_string(path).unwrap();
+            // TODO: still don't know how to solve this!
+            let decoded: Config = toml::from_str(&content).unwrap_or_else(|v| {
+                println!("{}", v);
+                Config::default()
+            });
             println!("eeeee {:#?}", decoded);
-            toml::from_str(&content).unwrap()
+            decoded
+            //toml::from_str(&content).unwrap()
         } else {
             Config::default()
         }
     }
+
+    fn load() -> Self {
+        let base_dir_buffer = dirs::home_dir().unwrap();
+        let base_dir = base_dir_buffer.to_str().unwrap();
+
+        let path = format!("{}/.rio/config.toml", base_dir);
+        if std::path::Path::new(&path).exists() {
+            let content = std::fs::read_to_string(path).unwrap();
+            // TODO: still don't know how to solve this!
+            let decoded: Config = toml::from_str(&content).unwrap_or_else(|v| {
+                println!("{}", v);
+                Config::default()
+            });
+            println!("eeeee {:#?}", decoded);
+            decoded
+            //toml::from_str(&content).unwrap()
+        } else {
+            Config::default()
+        }
+    }
+}
+
+fn create_temporary_config(performance: Performance, width: u16, height: u16) -> Config {
+    let toml_str = format!(
+        r#"
+        # Rio configuration file
+
+        # <performance> Set WGPU rendering performance
+        # default: High
+        # options: High, Average, Low
+        performance = "{:?}"
+
+        # <height> Set default height
+        # default: 400
+        height = {}
+
+        # <width> Set default width
+        # default: 600
+        width = {} 
+
+        ## TODO: Add more configs
+        "#,
+        performance, height, width
+    );
+    let binding = format!("/tmp/{:?}-config.toml", performance).to_owned();
+    let file_name = binding.as_str();
+
+    let mut file = std::fs::File::create(file_name).unwrap();
+    writeln!(file, "{}", toml_str).unwrap(); // writing using the macro 'writeln!'``
+
+    Config::load_from_path(file_name) // load_from_path should just call load() with a custom path
 }
 
 impl Default for Config {
@@ -52,8 +106,6 @@ impl Default for Config {
 
 #[cfg(test)]
 mod tests {
-    use std::io::Write;
-
     use super::*;
 
     #[test]
@@ -64,42 +116,27 @@ mod tests {
             height: 200,
         };
 
-        let result = Config::load();
+        let result = create_temporary_config(
+            expected.performance,
+            expected.width,
+            expected.height,
+        );
         assert_eq!(result, expected);
     }
 
     #[test]
     fn load_default_performance_config() {
-        let toml_str = r#"
-        # Rio configuration file
-
-        # <performance> Set WGPU rendering performance
-        # default: High
-        # options: High, Average, Low
-        performance = "High"
-
-        # <height> Set default height
-        # default: 400
-        height = 200 
-
-        # <width> Set default width
-        # default: 600
-        width = 300 
-
-        ## TODO: Add more configs
-        "#;
-
-        // TODO: improve this shit
-        let mut file = std::fs::File::create("/Users/hungaro/.rio/config.toml").unwrap();
-        write!(file, "{}", toml_str).expect("err");
-
         let expected = Config {
-            performance: Performance::High,
-            width: 300,
-            height: 200,
+            performance: Performance::Average,
+            width: 400,
+            height: 400,
         };
 
-        let result = Config::load();
+        let result = create_temporary_config(
+            expected.performance,
+            expected.width,
+            expected.height,
+        );
         assert_eq!(result, expected);
     }
 }
