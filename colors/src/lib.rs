63beab0874fc64a27f355a8bd8ab1f542b772201
Raphael Amorim <rapha850@gmail.com>
2023-01-26 10:58:30 +0100

    early implementation

--- colors/src/lib.rs
+++ colors/src/lib.rs
@@ -1,30 +1,34 @@
 use regex::Regex;
+use std::num::ParseIntError;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Rgba {
     red: f32,
     green: f32,
     blue: f32,
-    alpha: f32
+    alpha: f32,
 }
 
 impl Rgba {
+    #[allow(dead_code)]
     fn new(red: f32, green: f32, blue: f32, alpha: f32) -> Self {
         Self {
             red,
             green,
             blue,
-            alpha
+            alpha,
         }
     }
 
     pub fn from_hex(mut hex: String) -> Result<Self, String> {
+        let mut alpha: f32 = 1.0;
         let _match3or4_hex = "#?[a-f\\d]{3}[a-f\\d]?";
         let _match6or8_hex = "#?[a-f\\d]{6}([a-f\\d]{2})?";
         let non_hex_chars = Regex::new(r"(?i)[^#a-f\\0-9]").unwrap();
 
         // ^#?[a-f\\d]{3}[a-f\\d]?$|^#?[a-f\\d]{6}([a-f\\d]{2})?$ , "i"
-        let valid_hex_size = Regex::new(r"(?i)^#?[a-f\\0-9]{6}([a-f]\\0-9]{2})?$").unwrap();
+        let valid_hex_size =
+            Regex::new(r"(?i)^#?[a-f\\0-9]{6}([a-f]\\0-9]{2})?$").unwrap();
 
         if non_hex_chars.is_match(&hex) {
             return Err(String::from("Error: Character is not valid"));
@@ -34,18 +38,15 @@ impl Rgba {
             return Err(String::from("Error: Hex String size is not valid"));
         }
 
-        hex = hex.replace("#", "");
-        let mut alpha_from_hex: i32 = 1;
+        hex = hex.replace('#', "");
 
-        if hex.len() == 6 {
+        if hex.len() == 8 {
             // split_at(6, 8)
-            println!("{:?}", hex);
-            // let alpha = hex.split_at(6).1.to_string();
-            // let alpha_i32:i32 = alpha.parse::<i32>().unwrap(); 
-            // alpha_from_hex = alpha_i32 / 255;
+            let items = hex.split_at(4);
+            let alpha_from_hex = items.1.to_string().parse::<i32>().unwrap();
+            hex = items.0.to_string();
+            alpha = (alpha_from_hex / 255) as f32;
             // hex = hex.split_at(1).0.to_string();
-
-            println!("{:?}", hex)
         }
 
         // if hex.len() == 4 {
@@ -53,74 +54,64 @@ impl Rgba {
         //  hex = hex.slice(0, 3);
         // }
 
-        // if hex.len() == 4 {
+        // if hex.len() == 3 {
         //  hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
         // }
 
-        let number = hex.parse::<i32>().unwrap();
+        let rgb = decode_hex(&hex).unwrap_or_default();
+        if rgb.is_empty() || rgb.len() > 4 {
+            return Err(String::from("Error: Invalid string, not able to convert"));
+        }
 
-        println!("{:?}", number);
-        let red = number >> 16;
-        let green = (number >> 8) & 255;
-        let blue = number & 255;
+        // let number = hex.parse::<i32>().unwrap();
+        // let red = number >> 16;
+        // let green = (number >> 8) & 255;
+        // let blue = number & 255;
         // let alpha = typeof options.alpha === "number" ? options.alpha : alpha_from_hex;
 
-
         Ok(Self {
-            red: red as f32, green: green as f32, blue: blue as f32, alpha: 1.0
+            red: (rgb[0] as f32) / 1000.0,
+            green: (rgb[1] as f32) / 1000.0,
+            blue: (rgb[2] as f32) / 1000.0,
+            alpha,
         })
     }
 
-    pub fn to_string(&self) -> String {
-        std::format!("r: {:?}, g: {:?}, b: {:?}, a: {:?}", self.red, self.green, self.blue, self.alpha)
+    pub fn format_string(&self) -> String {
+        std::format!(
+            "r: {:?}, g: {:?}, b: {:?}, a: {:?}",
+            self.red,
+            self.green,
+            self.blue,
+            self.alpha
+        )
     }
+}
+
+fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
 }
 
 impl Default for Rgba {
     // #000000 Color Hex Black #000
     fn default() -> Self {
         Self {
-            red: 0.021, green: 0.021, blue: 0.021, alpha: 1.0
+            red: 0.021,
+            green: 0.021,
+            blue: 0.021,
+            alpha: 1.0,
         }
     }
 }
 
 impl std::fmt::Display for Rgba {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        std::fmt::Display::fmt(&self.to_string(), f)
+        std::fmt::Display::fmt(&self.format_string(), f)
     }
 }
-
-// export default function hexRgb(hex, options = {}) {
-//  if (typeof hex !== 'string' || nonHexChars.test(hex) || !validHexSize.test(hex)) {
-//      throw new TypeError('Expected a valid hex string');
-//  }
-
-//  hex = hex.replace(/^#/, '');
-//  let alphaFromHex = 1;
-
-//  if (hex.length === 8) {
-//      alphaFromHex = Number.parseInt(hex.slice(6, 8), 16) / 255;
-//      hex = hex.slice(0, 6);
-//  }
-
-//  if (hex.length === 4) {
-//      alphaFromHex = Number.parseInt(hex.slice(3, 4).repeat(2), 16) / 255;
-//      hex = hex.slice(0, 3);
-//  }
-
-//  if (hex.length === 3) {
-//      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-//  }
-
-//  const number = Number.parseInt(hex, 16);
-//  const red = number >> 16;
-//  const green = (number >> 8) & 255;
-//  const blue = number & 255;
-//  const alpha = typeof options.alpha === 'number' ? options.alpha : alphaFromHex;
-
-//  return {red, green, blue, alpha};
-// }
 
 #[cfg(test)]
 mod tests {
@@ -128,13 +119,10 @@ mod tests {
 
     #[test]
     fn conversion_from_hex_invalid_character() {
-        let invalid_character_color = match Rgba::from_hex(String::from("#invalid-color")) {
-            Ok(d) => {
-                d.to_string()
-            }
-            Err(e) => {
-                e
-            }
+        let invalid_character_color = match Rgba::from_hex(String::from("#invalid-color"))
+        {
+            Ok(d) => d.to_string(),
+            Err(e) => e,
         };
 
         assert_eq!(invalid_character_color, "Error: Character is not valid");
@@ -143,12 +131,8 @@ mod tests {
     #[test]
     fn conversion_from_hex_invalid_size() {
         let invalid_invalid_size = match Rgba::from_hex(String::from("abc")) {
-            Ok(d) => {
-                d.to_string()
-            }
-            Err(e) => {
-                e
-            }
+            Ok(d) => d.to_string(),
+            Err(e) => e,
         };
 
         assert_eq!(invalid_invalid_size, "Error: Hex String size is not valid");
@@ -156,9 +140,26 @@ mod tests {
 
     #[test]
     fn conversion_from_hex() {
+        let color = Rgba::from_hex(String::from("#151515")).unwrap();
+        assert_eq!(
+            color,
+            Rgba {
+                red: 0.021,
+                green: 0.021,
+                blue: 0.021,
+                alpha: 1.0
+            }
+        );
+
         let color = Rgba::from_hex(String::from("#000000")).unwrap();
-        assert_eq!(color, Rgba {
-            red: 0.021, green: 0.021, blue: 0.021, alpha: 1.0
-        });
+        assert_eq!(
+            color,
+            Rgba {
+                red: 0.0,
+                green: 0.0,
+                blue: 0.0,
+                alpha: 1.0
+            }
+        );
     }
 }
