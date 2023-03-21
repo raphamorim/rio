573d8944cc59edcd0ed8712754064d1e5f58099f
Raphael Amorim <rapha850@gmail.com>
2023-03-22 00:28:16 +0100

    update tests

--- config/src/defaults.rs
+++ config/src/defaults.rs
@@ -19,3 +19,25 @@ pub fn default_height() -> u16 {
 pub fn default_font_size() -> f32 {
     16.0
 }
+
+pub fn default_color_background() -> colors::Color {
+    colors::ColorBuilder::from_hex(String::from("#151515"), colors::Format::SRGB0_1)
+        .unwrap()
+        .to_wgpu()
+}
+
+pub fn default_color_tabs_active() -> [f32; 4] {
+    colors::ColorBuilder::from_hex(String::from("#F8A145"), colors::Format::SRGB0_1)
+        .unwrap()
+        .to_arr()
+}
+
+pub fn default_color_foreground() -> [f32; 4] {
+    [1.0, 1.0, 1.0, 1.0]
+}
+
+pub fn default_color_cursor() -> colors::Color {
+    colors::ColorBuilder::from_hex(String::from("#8E12CC"), colors::Format::SRGB0_1)
+        .unwrap()
+        .to_wgpu()
+}
