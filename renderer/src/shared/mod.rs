95a85825bec9931c28910e99e8805064e6646e34
Raphael Amorim <rapha850@gmail.com>
2023-03-21 12:42:22 +0100

    start renderer rewrite

--- renderer/src/shared/mod.rs
+++ renderer/src/shared/mod.rs
@@ -1,5 +1,2 @@
 pub const FONT_FIRAMONO: &[u8; 170204] =
     include_bytes!("./fonts/FiraMono/FiraMono-Regular.ttf");
-
-pub const FONT_NOVAMONO: &[u8; 299208] =
-    include_bytes!("./fonts/NovaMono/NovaMono-Regular.ttf");
