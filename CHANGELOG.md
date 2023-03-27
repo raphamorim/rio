2044c3a19546d802e640539225260306da224953
Raphael Amorim <rapha850@gmail.com>
2023-03-27 15:05:38 +0200

    {HOME}/.rio/rio.toml for macos and linux

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 # Rio 0.0.1
 
-- `Teletypewriter` 1.0.2 first release published.
+- `Teletypewriter` 1.0.5 usage for macos and linux.
--- config/src/lib.rs
+++ config/src/lib.rs
@@ -119,11 +119,11 @@ impl Config {
     }
 
     pub fn load_macos() -> Self {
-        // XDG base directory
-        let base_dir_buffer = dirs::config_dir().unwrap();
+        // Home directory
+        let base_dir_buffer = dirs::home_dir().unwrap();
         let base_dir = base_dir_buffer.to_str().unwrap();
 
-        let path = format!("{base_dir}/rio.toml");
+        let path = format!("{base_dir}/.rio/config.toml");
         if std::path::Path::new(&path).exists() {
             let content = std::fs::read_to_string(path).unwrap();
             match toml::from_str(&content) {
