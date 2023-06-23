8a3a3a1d8c840c424be3d8f99657288356c61774
Raphael Amorim <rapha850@gmail.com>
2023-06-23 19:29:13 +0200

    update style for win

--- rio/src/screen/constants.rs
+++ rio/src/screen/constants.rs
@@ -1,17 +1,17 @@
-#[cfg(not(any(target_os = "macos", windows)))]
+#[cfg(not(any(target_os = "macos")))]
 pub const PADDING_Y: f32 = 10.;
 
-#[cfg(any(target_os = "macos", windows))]
+#[cfg(any(target_os = "macos"))]
 pub const PADDING_Y: f32 = 30.;
 
-#[cfg(not(any(target_os = "macos", windows)))]
+#[cfg(not(any(target_os = "macos")))]
 pub const INACTIVE_TAB_WIDTH_SIZE: f32 = 4.;
 
-#[cfg(any(target_os = "macos", windows))]
+#[cfg(any(target_os = "macos"))]
 pub const INACTIVE_TAB_WIDTH_SIZE: f32 = 16.;
 
-#[cfg(not(any(target_os = "macos", windows)))]
+#[cfg(not(any(target_os = "macos")))]
 pub const ACTIVE_TAB_WIDTH_SIZE: f32 = 8.;
 
-#[cfg(any(target_os = "macos", windows))]
+#[cfg(any(target_os = "macos"))]
 pub const ACTIVE_TAB_WIDTH_SIZE: f32 = 26.;
