d8f8697c7a0114f98ec000a5782b051607d77113
Raphael Amorim <rapha850@gmail.com>
2023-09-07 10:33:20 +0200

    add top to other sections

--- docs/assets/javascript/app.js
+++ docs/assets/javascript/app.js
@@ -1,13 +1,14 @@
-let mybutton = document.getElementById("go-to-top");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+let mybutton = document.getElementById('go-to-top');
+if (typeof(mybutton) != 'undefined' && mybutton != null) {
+  window.onscroll = () => {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      mybutton.style.display = 'block';
+    } else {
+      mybutton.style.display = 'none';
+    }
   }
+  mybutton.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }, false);
 }
-mybutton.addEventListener('click', () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}, false);
