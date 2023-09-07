22fbcdc256db1de79680f6eae2b1764e50877774
Raphael Amorim <rapha850@gmail.com>
2023-09-07 11:32:10 +0200

    move create toolbar to ui::appkit::mod

--- rio/src/sequencer.rs
+++ rio/src/sequencer.rs
@@ -1,9 +1,4 @@
 #[cfg(target_os = "macos")]
-use crate::ui::appkit::{InitializedApplication, MenuBar, NSMenuItem};
-#[cfg(target_os = "macos")]
-use objc2::rc::autoreleasepool;
-
-#[cfg(target_os = "macos")]
 use winit::platform::macos::WindowExtMacOS;
 
 use crate::clipboard::ClipboardType;
@@ -337,69 +332,7 @@ impl Sequencer {
                     Event::NewEvents(StartCause::Init) => {
                         #[cfg(target_os = "macos")]
                         {
-                            autoreleasepool(|_pool| {
-                                let app = unsafe { InitializedApplication::new() };
-                                // let menubar = app.menubar(pool).unwrap();
-                                // // Yeah, this is not ok but we'll do it for now
-                                // let menubar: Id<NSMenu, Owned> =
-                                //     unsafe { Id::retain(NonNull::from(menubar)) };
-                                // let mut menubar = unsafe { MenuBar::from_raw(menubar) };
-                                let mut menubar = MenuBar::new(|menu| {
-                                    menu.add(NSMenuItem::new(
-                                        env!("CARGO_PKG_VERSION"),
-                                        "",
-                                        None,
-                                    ));
-                                    menu.add(NSMenuItem::new_separator());
-                                    menu.add(NSMenuItem::new("Hide Rio", "h", None));
-                                    menu.add(NSMenuItem::new("Quit Rio", "q", None));
-                                });
-
-                                menubar.add("Shell", |menu| {
-                                    menu.add(NSMenuItem::new(
-                                        "Will be above the window data",
-                                        "",
-                                        None,
-                                    ));
-                                });
-
-                                menubar.add("Edit", |menu| {
-                                    menu.add(NSMenuItem::new(
-                                        "Will be above the window data",
-                                        "",
-                                        None,
-                                    ));
-                                });
-
-                                menubar.add("View", |menu| {
-                                    menu.add(NSMenuItem::new(
-                                        "Will be above the window data",
-                                        "",
-                                        None,
-                                    ));
-                                });
-
-                                let window_menu = menubar.add("Window", |menu| {
-                                    menu.add(NSMenuItem::new(
-                                        "Will be above the window data",
-                                        "",
-                                        None,
-                                    ));
-                                });
-
-                                let help_menu = menubar.add("Help", |menu| {
-                                    menu.add(NSMenuItem::new("Item 2 : 1", "", None));
-                                    menu.add(NSMenuItem::new(
-                                        "Search or report issue on Github",
-                                        "",
-                                        None,
-                                    ));
-                                });
-
-                                app.set_window_menu(&window_menu);
-                                app.set_help_menu(Some(&help_menu));
-                                app.set_menubar(menubar);
-                            });
+                            crate::ui::appkit::create_toolbar();
                         }
                     }
 
