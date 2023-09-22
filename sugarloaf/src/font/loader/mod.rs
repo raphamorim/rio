de8d438598273fe10e3e23118e90f0ae39fa970a
Raphael Amorim <rapha850@gmail.com>
2023-09-22 08:15:44 +0200

    add fontconfig

--- sugarloaf/src/font/loader/mod.rs
+++ sugarloaf/src/font/loader/mod.rs
@@ -1,4 +1,5 @@
-// Originally retired from https://github.com/RazrFalcon/fontdb/blob/master/Cargo.toml
+// Originally retired from https://github.com/RazrFalcon/fontdb
+// Which is licensed with a MIT license https://github.com/RazrFalcon/fontdb/blob/da742a867f71ab43ee5eaf12f543f4cd404b67dd/LICENSE
 
 extern crate alloc;
 
@@ -291,22 +292,91 @@ impl Database {
         // Linux.
         #[cfg(all(unix, not(any(target_os = "macos", target_os = "android"))))]
         {
-            #[cfg(feature = "fontconfig")]
-            {
-                self.load_fontconfig();
+            self.load_fontconfig();
+        
+            self.load_fonts_dir("/usr/share/fonts/");
+            self.load_fonts_dir("/usr/local/share/fonts/");
+
+            if let Ok(ref home) = std::env::var("HOME") {
+                let home_path = std::path::Path::new(home);
+                self.load_fonts_dir(home_path.join(".fonts"));
+                self.load_fonts_dir(home_path.join(".local/share/fonts"));
             }
+        }
+    }
 
-            #[cfg(not(feature = "fontconfig"))]
-            {
-                self.load_fonts_dir("/usr/share/fonts/");
-                self.load_fonts_dir("/usr/local/share/fonts/");
+    #[cfg(all(
+        unix,
+        not(any(target_os = "macos", target_os = "android"))
+    ))]
+    fn load_fontconfig(&mut self) {
+        use std::path::Path;
 
-                if let Ok(ref home) = std::env::var("HOME") {
-                    let home_path = std::path::Path::new(home);
-                    self.load_fonts_dir(home_path.join(".fonts"));
-                    self.load_fonts_dir(home_path.join(".local/share/fonts"));
+        let mut fontconfig = fontconfig_parser::FontConfig::default();
+        let home = std::env::var("HOME");
+
+        if let Ok(ref config_file) = std::env::var("FONTCONFIG_FILE") {
+            let _ = fontconfig.merge_config(Path::new(config_file));
+        } else {
+            let xdg_config_home = if let Ok(val) = std::env::var("XDG_CONFIG_HOME") {
+                Some(val.into())
+            } else if let Ok(ref home) = home {
+                // according to https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+                // $XDG_CONFIG_HOME should default to $HOME/.config if not set
+                Some(Path::new(home).join(".config"))
+            } else {
+                None
+            };
+
+            let read_global = match xdg_config_home {
+                Some(p) => fontconfig
+                    .merge_config(&p.join("fontconfig/fonts.conf"))
+                    .is_err(),
+                None => true,
+            };
+
+            if read_global {
+                let _ = fontconfig.merge_config(Path::new("/etc/fonts/local.conf"));
+            }
+            let _ = fontconfig.merge_config(Path::new("/etc/fonts/fonts.conf"));
+        }
+
+        for fontconfig_parser::Alias {
+            alias,
+            default,
+            prefer,
+            accept,
+        } in fontconfig.aliases
+        {
+            let name = prefer
+                .get(0)
+                .or_else(|| accept.get(0))
+                .or_else(|| default.get(0));
+
+            if let Some(name) = name {
+                match alias.to_lowercase().as_str() {
+                    "serif" => self.set_serif_family(name),
+                    "sans-serif" => self.set_sans_serif_family(name),
+                    "sans serif" => self.set_sans_serif_family(name),
+                    "monospace" => self.set_monospace_family(name),
+                    "cursive" => self.set_cursive_family(name),
+                    "fantasy" => self.set_fantasy_family(name),
+                    _ => {}
                 }
             }
+        }
+
+        for dir in fontconfig.dirs {
+            let path = if dir.path.starts_with("~") {
+                if let Ok(ref home) = home {
+                    Path::new(home).join(dir.path.strip_prefix("~").unwrap())
+                } else {
+                    continue;
+                }
+            } else {
+                dir.path
+            };
+            self.load_fonts_dir(path);
         }
     }
 
