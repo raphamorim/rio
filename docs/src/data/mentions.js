666a79be956c427f82f71128ef442a9d339210a1
Raphael Amorim <rapha850@gmail.com>
2023-09-23 19:31:58 +0200

    update docs

--- docs/src/data/mentions.js
+++ docs/src/data/mentions.js
@@ -8,11 +8,37 @@ const MENTIONS = [
     date: 'May 23, 2023',
     content: (
       <>
-       This week I'm testing rio, a rust based terminal and I need to say: It's amazing! either still beta, could full fill all need working heavily with git/docker/spring/npm , I really recommend to get a try and Kudos to @raphamorims for fantastic job
+        This week I'm testing rio, a rust based terminal and I need to say: It's amazing! either still beta, could full fill all need working heavily with git/docker/spring/npm , I really recommend to get a try and Kudos to @raphamorims for fantastic job
       </>
     ),
     showOnHomepage: true,
     githubUsername: 'matheusmonte',
+  },
+  {
+    url: 'https://twitter.com/moeyua13/status/1665983183526195200',
+    handle: 'moeyua13',
+    name: 'Moeyua',
+    date: 'Jun 6, 2023',
+    content: (
+      <>
+        I really love this terminal
+      </>
+    ),
+    showOnHomepage: true,
+    githubUsername: 'moeyua',
+  },
+  {
+    url: 'https://twitter.com/peterlazar1993/status/1689965895408402432',
+    handle: 'peterlazar1993',
+    name: 'Peter Lazar',
+    date: 'Aug 11, 2023',
+    content: (
+      <>
+        I've been dailydriving Rio, it's 🔥
+      </>
+    ),
+    showOnHomepage: true,
+    githubUsername: 'peterlazar1993',
   },
 ];
 
