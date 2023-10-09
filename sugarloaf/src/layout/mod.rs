bded6bc94da2c6c8355de4b83437ee905f433bb5
Raphael Amorim <rapha850@gmail.com>
2023-10-09 10:44:12 +0200

    remove round from spaces

--- sugarloaf/src/layout/mod.rs
+++ sugarloaf/src/layout/mod.rs
@@ -55,20 +55,11 @@ fn compute(
     margin: Delta<f32>,
     min_cols_lines: (usize, usize),
 ) -> (usize, usize) {
-    // println!("{:?}", margin);
     let margin_x = ((margin.x) * scale_factor).floor();
-    // let margin_y = ((margin.y) * scale_factor).floor();
-    // TODO: Figure out margin calculations
-    // topy: 15. and bottomy: 5.
-    let margin_spaces = (margin.top_y * 2.).round() + (margin.bottom_y * 2.).round();
+    let margin_spaces = (margin.top_y * 2.) + (margin.bottom_y * 2.);
 
     let mut lines = (dimensions.1 / scale_factor) - margin_spaces;
-    // println!("available space {:?}", lines);
-    // println!("fonts now {:?}", font_bounds.1 * line_height);
     lines /= font_bounds.1 * line_height;
-    // println!("result {:?}", lines);
-    // lines = lines - ((font_bounds.1 * line_height) / margin.top_y);
-
     let visible_lines = std::cmp::max(lines.floor() as usize, min_cols_lines.1);
 
     let mut visible_columns = (dimensions.0 / scale_factor) - margin_x;
