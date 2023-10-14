1446e69533744fb8daba171f063159a852cccaa1
Raphael Amorim <rapha850@gmail.com>
2023-10-14 12:41:30 +0200

    move logic to mouse.rs and prepare to start writing tests (#182)

--- rio/src/screen/mouse.rs
+++ rio/src/screen/mouse.rs
@@ -1,5 +1,8 @@
+use crate::crosswords::pos::Column;
+use crate::crosswords::pos::Line;
 use crate::crosswords::pos::Side;
 use crate::event::ClickState;
+use crate::screen::Pos;
 use std::time::Instant;
 use winit::event::ElementState;
 use winit::event::MouseButton;
@@ -49,3 +52,39 @@ impl Default for Mouse {
         }
     }
 }
+
+#[inline]
+pub fn calculate_mouse_position(
+    mouse: &Mouse,
+    display_offset: usize,
+    scale_factor: f32,
+    config_columns_rows: (usize, usize),
+    margin_x: f32,
+    margin_y: (f32, f32),
+    cell_dimension: (f32, f32),
+) -> Pos {
+    let mouse_x_f32 = mouse.x as f32;
+    let scaled_margin_x = margin_x * scale_factor;
+    // println!("mouse_x_f32 {:?}", mouse_x_f32);
+    // println!("layout.margin.x {:?}", layout.margin.x);
+
+    let col: Column = if scaled_margin_x >= mouse_x_f32 {
+        Column(0)
+    } else {
+        let col = ((mouse_x_f32 - margin_x) / cell_dimension.0).floor() as usize;
+        std::cmp::min(Column(col), Column(config_columns_rows.0))
+    };
+
+    // println!("{:?}", col);
+
+    let row = mouse
+        .y
+        .saturating_sub((margin_y.0 * 2. * scale_factor) as usize)
+        / cell_dimension.1 as usize;
+    let calc_row = std::cmp::min(row, config_columns_rows.1 - 1);
+    let row = Line(calc_row as i32) - (display_offset);
+
+    Pos::new(row, col)
+}
+
+// TODO: Write down calculate_mouse_position tests
