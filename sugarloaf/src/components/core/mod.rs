62082f31a272926d374f3d4ba592f99d68f9a777
TornaxO7 <tornax@proton.me>
2023-10-14 21:11:05 +0200

    format: reformat list to matrix

--- sugarloaf/src/components/core/mod.rs
+++ sugarloaf/src/components/core/mod.rs
@@ -4,23 +4,15 @@ pub mod buffer;
 pub mod shapes;
 
 #[inline]
+#[rustfmt::skip]
 pub fn orthographic_projection(width: u32, height: u32) -> [f32; 16] {
+    let h = height as f32;
+    let w = width as f32;
+
     [
-        2.0 / width as f32,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        -2.0 / height as f32,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        1.0,
-        0.0,
-        -1.0,
-        1.0,
-        0.0,
-        1.0,
+        2.0 / w, 0.0,      0.0, 0.0,
+        0.0,     -2.0 / h, 0.0, 0.0,
+        0.0,     0.0,      1.0, 0.0,
+        -1.0,    1.0,      0.0, 1.0,
     ]
 }
