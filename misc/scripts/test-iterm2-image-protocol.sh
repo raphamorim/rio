d5202009f779a11ebfa86d2fa5d0d34ef7d3d67b
Raphael Amorim <rapha850@gmail.com>
2023-10-22 12:30:50 +0200

    update test script

--- misc/scripts/test-iterm2-image-protocol.sh
+++ misc/scripts/test-iterm2-image-protocol.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-bash ./misc/scripts/utils/imgcat.sh -W 100px -H 100px -s ./misc/osx/rio-logo.png
+bash ./misc/scripts/utils/imgcat.sh -W 250px -H 250px -s ./misc/osx/rio-logo.png
+bash ./misc/scripts/utils/imgcat.sh -W 50px -H 50px -s ./misc/osx/rio-logo.png
