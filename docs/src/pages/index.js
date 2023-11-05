965edd52db4c05d88d07b47511cb9437ae3ee059
Han Yeong-woo <han@yeongwoo.dev>
2023-11-06 08:31:16 +0900

    Bit improve docs (#330)
    * Add docs typing settings
    
    * Fix duplicate title
    
    * Remove unnecessary file

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,13 +1,15 @@
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Mention from '@site/src/components/Mention';
 import Mentions from '@site/src/data/mentions';
+import Heading from '@theme/Heading';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
 
 import styles from './index.module.css';
+
+const title = 'Meet Rio';
 
 const Logo = ({ src }) => (
   <div className="logo">
@@ -28,7 +30,9 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Logo src={'assets/logo.svg'} />
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title">
+          {title} | {siteConfig.title}
+        </h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
@@ -70,10 +74,9 @@ function MentionsSection() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title={title}
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
