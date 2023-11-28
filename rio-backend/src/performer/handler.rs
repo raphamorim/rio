7a017064e5874c69253795a4cd6f21136c64cd9d
Raphael Amorim <rapha850@gmail.com>
2023-11-28 17:25:02 +0100

    disable sugarloaf warn errors when find the font

--- rio-backend/src/performer/handler.rs
+++ rio-backend/src/performer/handler.rs
@@ -723,6 +723,35 @@ impl<U: Handler> copa::Perform for Performer<'_, U> {
             // Reset text cursor color.
             b"112" => self.handler.reset_color(NamedColor::Cursor as usize),
 
+            // (OSC 1337 is not only used by iTerm2 protocol)
+            // OSC 1337 is equal to xterm’s OSC 50
+            b"1337" => {
+                // \x1b]1337;File=[arguments]:[base-64 encoded file contents]^G
+                //
+                // Example:
+                // printf "\x1b]1337;File=;size=234:aGVsbG8=\x07"
+                //
+                // leads to
+                // name: None,
+                // size: Some(234),
+                // width: Automatic,
+                // height: Automatic,
+                // preserve_aspect_ratio: true,
+                // inline: false,
+                // do_not_move_cursor: false,
+                // data: b"hello".to_vec(),
+
+                if params.len() >= 2
+                    && params[1].len() >= 5
+                    && params[1][0..5] == *b"File="
+                {
+                    println!("entrou");
+                    // self.handler.set_cursor_shape(shape);
+                    return;
+                }
+                unhandled(params);
+            }
+
             _ => unhandled(params),
         }
     }
