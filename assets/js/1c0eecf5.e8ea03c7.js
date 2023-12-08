d29eec827c8a1453b1755793bdba60c36a9b7ff2
raphamorim <raphamorim@users.noreply.github.com>
2023-12-08 20:53:34 +0000

    deploy: 8c9b323f948e08c28f8380309d0ba81f49b8bd67

--- assets/js/1c0eecf5.e8ea03c7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkrio_docs=self.webpackChunkrio_docs||[]).push([[9032],{4469:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ pt-br/assets/js/458ed05a.2813951d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrio_docs=self.webpackChunkrio_docs||[]).push([[9496],{3769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
