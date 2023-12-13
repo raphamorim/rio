b9c1732a9e26569a2beb9d63eebe9e5f1cc33e9a
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-12-13 18:42:57 +0100

    fix: use explicit lifetime for const str (#385)

--- rio-backend/src/config/navigation.rs
+++ rio-backend/src/config/navigation.rs
@@ -29,14 +29,14 @@ impl Default for NavigationMode {
 }
 
 impl NavigationMode {
-    const PLAIN_STR: &str = "Plain";
-    const COLLAPSED_TAB_STR: &str = "CollapsedTab";
-    const TOP_TAB_STR: &str = "TopTab";
-    const BOTTOM_TAB_STR: &str = "BottomTab";
+    const PLAIN_STR: &'static str = "Plain";
+    const COLLAPSED_TAB_STR: &'static str = "CollapsedTab";
+    const TOP_TAB_STR: &'static str = "TopTab";
+    const BOTTOM_TAB_STR: &'static str = "BottomTab";
     #[cfg(target_os = "macos")]
-    const NATIVE_TAB_STR: &str = "NativeTab";
+    const NATIVE_TAB_STR: &'static str = "NativeTab";
     #[cfg(not(windows))]
-    const BREADCRUMB_STR: &str = "Breadcrumb";
+    const BREADCRUMB_STR: &'static str = "Breadcrumb";
 
     pub fn as_str(&self) -> &'static str {
         match self {
