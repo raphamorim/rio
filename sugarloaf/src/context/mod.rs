9550ac8d3bc31d1cac67684db96c981216a5a031
Raphael Amorim <rapha850@gmail.com>
2023-12-16 21:18:47 +0100

    use Bgra8Unorm on MacOS

--- sugarloaf/src/context/mod.rs
+++ sugarloaf/src/context/mod.rs
@@ -11,6 +11,44 @@ pub struct Context {
     pub adapter_info: wgpu::AdapterInfo,
 }
 
+#[inline]
+fn find_best_texture_format(formats: Vec<wgpu::TextureFormat>) -> wgpu::TextureFormat {
+    let mut format: wgpu::TextureFormat = formats.first().unwrap().to_owned();
+
+    // TODO: Fix formats with signs
+    // FIXME: On Nvidia GPUs usage Rgba16Float texture format causes driver to enable HDR.
+    // Reason for this is currently output color space is poorly defined in wgpu and
+    // anything other than Srgb texture formats can cause undeterministic output color
+    // space selection which also causes colors to mismatch. Optionally we can whitelist
+    // only the Srgb texture formats for now until output color space selection lands in wgpu. See #205
+    // TODO: use output color format for the CanvasConfiguration when it lands on the wgpu
+    #[cfg(windows)]
+    let unsupported_formats = [
+        wgpu::TextureFormat::Rgba8Snorm,
+        wgpu::TextureFormat::Rgba16Float,
+    ];
+
+    // not reproduce-able on mac
+    #[cfg(not(windows))]
+    let unsupported_formats = [wgpu::TextureFormat::Rgba8Snorm];
+
+    let filtered_formats: Vec<wgpu::TextureFormat> = formats
+        .iter()
+        .copied()
+        .filter(|&x| {
+            !wgpu::TextureFormat::is_srgb(&x) && !unsupported_formats.contains(&x)
+        })
+        .collect();
+
+    if !filtered_formats.is_empty() {
+        format = filtered_formats.first().unwrap().to_owned();
+    }
+
+    log::info!("Sugarloaf selected format: {format:?} from {:?}", formats);
+
+    format
+}
+
 impl Context {
     pub async fn new(
         sugarloaf_window: &SugarloafWindow,
@@ -63,41 +101,11 @@ impl Context {
 
         let caps = surface.get_capabilities(&adapter);
 
-        // TODO: Fix formats with signs
-        // FIXME: On Nvidia GPUs usage Rgba16Float texture format causes driver to enable HDR.
-        // Reason for this is currently output color space is poorly defined in wgpu and
-        // anything other than Srgb texture formats can cause undeterministic output color
-        // space selection which also causes colors to mismatch. Optionally we can whitelist
-        // only the Srgb texture formats for now until output color space selection lands in wgpu. See #205
-        // TODO: use output color format for the CanvasConfiguration when it lands on the wgpu
-        #[cfg(windows)]
-        let unsupported_formats = [
-            wgpu::TextureFormat::Rgba8Snorm,
-            wgpu::TextureFormat::Rgba16Float,
-        ];
+        #[cfg(target_os = "macos")]
+        let format = wgpu::TextureFormat::Bgra8Unorm;
+        #[cfg(not(target_os = "macos"))]
+        let format = find_best_texture_format(caps.formats);
 
-        // not reproduce-able on mac
-        #[cfg(not(windows))]
-        let unsupported_formats = [wgpu::TextureFormat::Rgba8Snorm];
-
-        let filtered_formats: Vec<wgpu::TextureFormat> = caps
-            .formats
-            .iter()
-            .copied()
-            .filter(|&x| {
-                !wgpu::TextureFormat::is_srgb(&x) && !unsupported_formats.contains(&x)
-            })
-            .collect();
-
-        let mut format: wgpu::TextureFormat = caps.formats.first().unwrap().to_owned();
-        if !filtered_formats.is_empty() {
-            format = filtered_formats.first().unwrap().to_owned();
-        }
-
-        log::info!(
-            "Sugarloaf selected format: {format:?} from {:?}",
-            caps.formats
-        );
         let (device, queue) = (async {
             {
                 if let Ok(result) = adapter
