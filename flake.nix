c9ccd3d4d14d1fd37aa0707ddbb3665d2b73ce85
TornaxO7 <tornax@proton.me>
2024-01-13 19:59:45 +0100

    flake: add `apps` entry and fix runtime error
    flake: fix package creation

--- flake.nix
+++ flake.nix
@@ -31,50 +31,78 @@
           extensions = ["rust-src" "rust-analyzer"];
         };
 
-        runtimeDeps = with pkgs;
-          if stdenv.isDarwin
-          then [
-            darwin.libobjc
-            darwin.apple_sdk_11_0.frameworks.AppKit
-            darwin.apple_sdk_11_0.frameworks.AVFoundation
-            darwin.apple_sdk_11_0.frameworks.Vision
-          ]
-          else
-            (with pkgs; [
-              (lib.getLib gcc-unwrapped)
-              fontconfig
-              libGL
-              libxkbcommon
-              vulkan-loader
-              wayland
-            ])
-            ++ (with pkgs.xorg; [
-              libX11
-              libXcursor
-              libXi
-              libXrandr
-              libxcb
-            ]);
-
-        buildDeps = with pkgs;
-          [
-            ncurses
-          ]
-          ++ lib.optionals stdenv.isLinux [
-            pkg-config
-            cmake
-            autoPatchelfHook
-          ];
-
-        rustPackage = rust-toolchain:
-          pkgs.rustPlatform.buildRustPackage {
+        mkRio = {
+          rustPlatform,
+          stdenv,
+          lib,
+          fontconfig,
+          darwin,
+          gcc-unwrapped,
+          libGL,
+          libxkbcommon,
+          vulkan-loader,
+          libX11,
+          libXcursor,
+          libXi,
+          libXrandr,
+          libxcb,
+          wayland,
+          ncurses,
+          pkg-config,
+          cmake,
+          autoPatchelfHook,
+          withX11 ? !stdenv.isDarwin,
+          withWayland ? !stdenv.isDarwin,
+          ...
+        }: let
+          rlinkLibs =
+            if stdenv.isDarwin
+            then [
+              darwin.libobjc
+              darwin.apple_sdk_11_0.frameworks.AppKit
+              darwin.apple_sdk_11_0.frameworks.AVFoundation
+              darwin.apple_sdk_11_0.frameworks.Vision
+            ]
+            else
+              [
+                (lib.getLib gcc-unwrapped)
+                fontconfig
+                libGL
+                libxkbcommon
+                vulkan-loader
+              ]
+              ++ lib.optionals withX11 [
+                libX11
+                libXcursor
+                libXi
+                libXrandr
+                libxcb
+              ]
+              ++ lib.optionals withWayland [
+                wayland
+              ];
+        in
+          rustPlatform.buildRustPackage {
             inherit (cargoToml.workspace.package) version;
             name = "rio";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
+
             cargoBuildFlags = "-p rioterm";
-            buildInputs = runtimeDeps ++ buildDeps;
-            nativeBuildInputs = buildDeps;
+
+            buildInputs = rlinkLibs;
+            runtimeDependencies = rlinkLibs;
+
+            nativeBuildInputs =
+              [
+                ncurses
+              ]
+              ++ lib.optionals stdenv.isLinux [
+                pkg-config
+                cmake
+                autoPatchelfHook
+              ];
+
             buildNoDefaultFeatures = true;
             buildFeatures = ["x11" "wayland"];
             meta = {
@@ -87,10 +115,12 @@
             };
           };
 
-        mkDevShell = rust-toolchain:
+        mkDevShell = rust-toolchain: let
+          dependencies = self'.packages.rio.nativeBuildInputs ++ self'.packages.rio.buildInputs;
+        in
           pkgs.mkShell {
-            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (runtimeDeps ++ buildDeps)}:$LD_LIBRARY_PATH";
-            packages = buildDeps ++ runtimeDeps ++ [rust-toolchain];
+            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath dependencies}:$LD_LIBRARY_PATH";
+            packages = dependencies ++ [rust-toolchain];
           };
       in {
         _module.args.pkgs = import inputs.nixpkgs {
@@ -102,7 +132,11 @@
         packages.default = self'.packages.rio;
         devShells.default = self'.devShells.msrv;
 
-        packages.rio = rustPackage "rio";
+        apps.default = {
+          type = "app";
+          program = self'.packages.rio;
+        };
+        packages.rio = pkgs.callPackage mkRio {};
 
         devShells.msrv = mkDevShell rust-toolchain;
         devShells.stable = mkDevShell pkgs.rust-bin.stable.latest.default;
