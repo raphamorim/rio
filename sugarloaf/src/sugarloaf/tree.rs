1c7ed7af7afafe4d4cd43d0894d2ffa1f61a0d53
Raphael Amorim <rapha850@gmail.com>
2024-02-04 01:37:05 +0100

    calculate_diff and SugarChanges

--- sugarloaf/src/sugarloaf/tree.rs
+++ sugarloaf/src/sugarloaf/tree.rs
@@ -1,14 +1,24 @@
+// use std::ops::Range;
 use crate::sugarloaf::SugarLine;
+use crate::Sugar;
 use fnv::FnvHashMap;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct SugarTreeChange {
+    line: usize,
+    column: usize,
+    before: Sugar,
+    after: Sugar,
+    // range: Range<usize>,
+    // content: Vec<char>,
+}
 
 #[derive(Debug, PartialEq)]
 pub enum SugarTreeDiff {
     Equal,
-    LineQuantityIsDifferent(i32),
-    LineChange(usize),
-    ColumunsChanged(usize),
-    InsertedCharacter,
-    DeletedCharacter,
+    LineLengthIsDifferent(i32),
+    ColumnsLengthIsDifferent(i32),
+    Changes(Vec<SugarTreeChange>),
 }
 
 // const LINE_MAX_LINES: usize = 140;
@@ -51,17 +61,35 @@ impl SugarTree {
     pub fn calculate_diff(&self, next: &SugarTree) -> SugarTreeDiff {
         let current_len = self.inner.len();
         let next_len = next.len();
+        let mut changes: Vec<SugarTreeChange> = vec![];
 
         if current_len == next_len {
-            for line_number in 0..self.len() {
-                let current_line: SugarLine = self.inner[&line_number];
+            for line_number in 0..current_len {
+                let line: SugarLine = self.inner[&line_number];
                 let next_line: SugarLine = next.inner[&line_number];
-                if current_line != next_line {
-                    return SugarTreeDiff::LineChange(line_number);
+                if line.len != next_line.len {
+                    return SugarTreeDiff::ColumnsLengthIsDifferent(
+                        line.len as i32 - next_line.len as i32,
+                    );
+                }
+
+                for column in 0..line.len {
+                    if line[column] != next_line[column] {
+                        changes.push(SugarTreeChange {
+                            line: line_number,
+                            column: column,
+                            before: line[column],
+                            after: next_line[column],
+                        });
+                    }
                 }
             }
+
+            if !changes.is_empty() {
+                return SugarTreeDiff::Changes(changes);
+            }
         } else {
-            return SugarTreeDiff::LineQuantityIsDifferent(
+            return SugarTreeDiff::LineLengthIsDifferent(
                 current_len as i32 - next_len as i32,
             );
         }
@@ -94,6 +122,8 @@ impl SugarTree {
 pub mod test {
     use super::*;
     use crate::Sugar;
+    use crate::SugarDecoration::Disabled;
+    use crate::SugarStyle;
 
     #[test]
     fn test_sugartree_calculate_is_empty() {
@@ -133,7 +163,7 @@ pub mod test {
     }
 
     #[test]
-    fn test_sugartree_calculate_diff_line_len_change() {
+    fn test_sugartree_calculate_diff_lines_length_is_different() {
         let mut sugartree_a = SugarTree::default();
         let mut sugartree_b = SugarTree::default();
 
@@ -141,14 +171,14 @@ pub mod test {
 
         assert_eq!(
             sugartree_a.calculate_diff(&sugartree_b),
-            SugarTreeDiff::LineQuantityIsDifferent(1)
+            SugarTreeDiff::LineLengthIsDifferent(1)
         );
 
         sugartree_a.insert(1, SugarLine::default());
 
         assert_eq!(
             sugartree_a.calculate_diff(&sugartree_b),
-            SugarTreeDiff::LineQuantityIsDifferent(2)
+            SugarTreeDiff::LineLengthIsDifferent(2)
         );
 
         sugartree_b.insert(0, SugarLine::default());
@@ -157,12 +187,12 @@ pub mod test {
 
         assert_eq!(
             sugartree_a.calculate_diff(&sugartree_b),
-            SugarTreeDiff::LineQuantityIsDifferent(-1)
+            SugarTreeDiff::LineLengthIsDifferent(-1)
         );
     }
 
     #[test]
-    fn test_sugartree_calculate_diff_exact_change() {
+    fn test_sugartree_calculate_diff_columns_length_is_different() {
         let mut sugartree_a = SugarTree::default();
         let mut sugartree_b = SugarTree::default();
 
@@ -185,7 +215,139 @@ pub mod test {
 
         assert_eq!(
             sugartree_a.calculate_diff(&sugartree_b),
-            SugarTreeDiff::LineChange(0)
+            SugarTreeDiff::ColumnsLengthIsDifferent(-1)
+        );
+
+        sugartree_b.line_mut(0).unwrap().insert(Sugar {
+            content: 'c',
+            ..Sugar::default()
+        });
+
+        assert_eq!(
+            sugartree_a.calculate_diff(&sugartree_b),
+            SugarTreeDiff::ColumnsLengthIsDifferent(-2)
+        );
+
+        sugartree_a.line_mut(0).unwrap().insert(Sugar {
+            content: 'z',
+            ..Sugar::default()
+        });
+        sugartree_a.line_mut(0).unwrap().insert(Sugar {
+            content: 't',
+            ..Sugar::default()
+        });
+        sugartree_a.line_mut(0).unwrap().insert(Sugar {
+            content: 'o',
+            ..Sugar::default()
+        });
+
+        assert_eq!(
+            sugartree_a.calculate_diff(&sugartree_b),
+            SugarTreeDiff::ColumnsLengthIsDifferent(1)
+        );
+    }
+
+    #[test]
+    fn test_sugartree_calculate_diff_chages() {
+        let mut sugartree_a = SugarTree::default();
+        let mut sugartree_b = SugarTree::default();
+
+        sugartree_a.insert(0, SugarLine::default());
+        sugartree_a.line_mut(0).unwrap().insert(Sugar {
+            content: 'a',
+            ..Sugar::default()
+        });
+
+        sugartree_b.insert(0, SugarLine::default());
+        sugartree_b.line_mut(0).unwrap().insert(Sugar {
+            content: 'b',
+            ..Sugar::default()
+        });
+
+        let mut changes = vec![SugarTreeChange {
+            line: 0,
+            column: 0,
+            before: Sugar {
+                content: 'a',
+                foreground_color: [0.0, 0.0, 0.0, 0.0],
+                background_color: [0.0, 0.0, 0.0, 0.0],
+                style: SugarStyle {
+                    is_italic: false,
+                    is_bold: false,
+                    is_bold_italic: false,
+                },
+                decoration: Disabled,
+                cursor: None,
+                custom_decoration: None,
+                media: None,
+            },
+            after: Sugar {
+                content: 'b',
+                foreground_color: [0.0, 0.0, 0.0, 0.0],
+                background_color: [0.0, 0.0, 0.0, 0.0],
+                style: SugarStyle {
+                    is_italic: false,
+                    is_bold: false,
+                    is_bold_italic: false,
+                },
+                decoration: Disabled,
+                cursor: None,
+                custom_decoration: None,
+                media: None,
+            },
+        }];
+
+        assert_eq!(
+            sugartree_a.calculate_diff(&sugartree_b),
+            SugarTreeDiff::Changes(changes.clone())
+        );
+
+        sugartree_a.line_mut(0).unwrap().insert(Sugar {
+            content: 'k',
+            ..Sugar::default()
+        });
+
+        sugartree_b.line_mut(0).unwrap().insert(Sugar {
+            content: 'z',
+            ..Sugar::default()
+        });
+
+        changes.push(SugarTreeChange {
+            line: 0,
+            column: 1,
+            before: Sugar {
+                content: 'k',
+                foreground_color: [0.0, 0.0, 0.0, 0.0],
+                background_color: [0.0, 0.0, 0.0, 0.0],
+                style: SugarStyle {
+                    is_italic: false,
+                    is_bold: false,
+                    is_bold_italic: false,
+                },
+                decoration: Disabled,
+                cursor: None,
+                custom_decoration: None,
+                media: None,
+            },
+            after: Sugar {
+                content: 'z',
+                foreground_color: [0.0, 0.0, 0.0, 0.0],
+                background_color: [0.0, 0.0, 0.0, 0.0],
+                style: SugarStyle {
+                    is_italic: false,
+                    is_bold: false,
+                    is_bold_italic: false,
+                },
+                decoration: Disabled,
+                cursor: None,
+                custom_decoration: None,
+                media: None,
+            },
+        });
+
+        assert_eq!(
+            sugartree_a.calculate_diff(&sugartree_b),
+            SugarTreeDiff::Changes(changes)
         );
     }
 }
