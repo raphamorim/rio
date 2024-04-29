10e33ea6401430ef3f5286057456e8d85d72fe92
Raphael Amorim <rapha850@gmail.com>
2024-04-29 22:24:04 +0200

    update wgpu to 0.20.0

--- sugarloaf/src/components/rich_text/mod.rs
+++ sugarloaf/src/components/rich_text/mod.rs
@@ -224,6 +224,7 @@ impl RichTextBrush {
             label: None,
             layout: Some(&pipeline_layout),
             vertex: wgpu::VertexState {
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
                 module: &shader,
                 entry_point: "vs_main",
                 buffers: &[wgpu::VertexBufferLayout {
@@ -238,6 +239,7 @@ impl RichTextBrush {
                 }],
             },
             fragment: Some(wgpu::FragmentState {
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
                 module: &shader,
                 entry_point: "fs_main",
                 targets: &[Some(wgpu::ColorTargetState {
