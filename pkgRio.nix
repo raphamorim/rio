c3d5b0aef8e6a4ebe875a828a072ff301f08fef4
Zachary Myers <zackmyers@lavabit.com>
2024-04-30 16:17:43 -0400

    fix nix builds (#498)

--- pkgRio.nix
+++ pkgRio.nix
@@ -1,28 +1,33 @@
-{ rustPlatform
-, stdenv
-, lib
-, fontconfig
-, darwin
-, gcc-unwrapped
-, libGL
-, libxkbcommon
-, vulkan-loader
-, libX11
-, libXcursor
-, libXi
-, libXrandr
-, libxcb
-, wayland
-, ncurses
-, pkg-config
-, cmake
-, autoPatchelfHook
-, withX11 ? !stdenv.isDarwin
-, withWayland ? !stdenv.isDarwin
-, ...
-}:
-let
+{
+  rust-toolchain,
+  makeRustPlatform,
+  stdenv,
+  lib,
+  fontconfig,
+  darwin,
+  gcc-unwrapped,
+  libGL,
+  libxkbcommon,
+  vulkan-loader,
+  libX11,
+  libXcursor,
+  libXi,
+  libXrandr,
+  libxcb,
+  wayland,
+  ncurses,
+  pkg-config,
+  cmake,
+  autoPatchelfHook,
+  withX11 ? !stdenv.isDarwin,
+  withWayland ? !stdenv.isDarwin,
+  ...
+}: let
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+  rustPlatform = makeRustPlatform {
+    cargo = rust-toolchain;
+    rustc = rust-toolchain;
+  };
   rlinkLibs =
     if stdenv.isDarwin
     then [
@@ -51,35 +56,35 @@ let
         wayland
       ];
 in
-rustPlatform.buildRustPackage {
-  inherit (cargoToml.workspace.package) version;
-  name = "rio";
-  src = ./.;
-  cargoLock.lockFile = ./Cargo.lock;
+  rustPlatform.buildRustPackage {
+    inherit (cargoToml.workspace.package) version;
+    name = "rio";
+    src = ./.;
+    cargoLock.lockFile = ./Cargo.lock;
 
-  cargoBuildFlags = "-p rioterm";
+    cargoBuildFlags = "-p rioterm";
 
-  buildInputs = rlinkLibs;
-  runtimeDependencies = rlinkLibs;
+    buildInputs = rlinkLibs;
+    runtimeDependencies = rlinkLibs;
 
-  nativeBuildInputs =
-    [
-      ncurses
-    ]
-    ++ lib.optionals stdenv.isLinux [
-      pkg-config
-      cmake
-      autoPatchelfHook
-    ];
+    nativeBuildInputs =
+      [
+        ncurses
+      ]
+      ++ lib.optionals stdenv.isLinux [
+        pkg-config
+        cmake
+        autoPatchelfHook
+      ];
 
-  buildNoDefaultFeatures = true;
-  buildFeatures = [ "x11" "wayland" ];
-  meta = {
-    description = "A hardware-accelerated GPU terminal emulator focusing to run in desktops and browsers";
-    homepage = "https://raphamorim.io/rio";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.unix;
-    changelog = "https://github.com/raphamorim/rio/blob/master/CHANGELOG.md";
-    mainProgram = "rio";
-  };
-}
+    buildNoDefaultFeatures = true;
+    buildFeatures = ["x11" "wayland"];
+    meta = {
+      description = "A hardware-accelerated GPU terminal emulator focusing to run in desktops and browsers";
+      homepage = "https://raphamorim.io/rio";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.unix;
+      changelog = "https://github.com/raphamorim/rio/blob/master/CHANGELOG.md";
+      mainProgram = "rio";
+    };
+  }
