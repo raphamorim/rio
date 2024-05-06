168c42524fc8c4de3613a0c4c1da846b12a388e3
Raphael Amorim <rapha850@gmail.com>
2024-05-06 23:13:26 +0200

    docs updates

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -126,7 +126,7 @@ const config = {
           {
             type: 'docsVersionDropdown',
             position: 'right',
-            dropdownItemsAfter: [{to: '/versions', label: 'All versions'}],
+            // dropdownItemsAfter: [{to: '/versions', label: 'All versions'}],
             dropdownActiveClassDisabled: true,
           },
           {
