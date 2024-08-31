0924b1e82fd659a3e94326c357625374830086ff
Raphael Amorim <rapha850@gmail.com>
2024-08-31 21:18:21 +0200

    atlas for text only (missing grow/cleanup)

--- sugarloaf/src/components/rich_text/image_cache/atlas.rs
+++ sugarloaf/src/components/rich_text/image_cache/atlas.rs
@@ -35,6 +35,7 @@ impl AtlasAllocator {
     /// coordinates of the allocated slot.
     pub fn allocate(&mut self, width: u16, height: u16) -> Option<(u16, u16)> {
         // Width is a hard constraint; make sure it fits.
+        println!("{} {}", width, self.width);
         if width > self.width {
             return None;
         }
