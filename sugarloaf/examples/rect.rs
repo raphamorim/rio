01385c6bf6cf656199701ccb80c4573e2a16da47
Raphael Amorim <rapha850@gmail.com>
2024-09-05 14:50:01 +0200

    remove sugarloaf benchmark and update examples to use rio-window

--- sugarloaf/examples/rect.rs
+++ sugarloaf/examples/rect.rs
@@ -1,121 +1,160 @@
-extern crate png;
-
 use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
+use rio_window::application::ApplicationHandler;
 use rio_window::event_loop::ControlFlow;
-use rio_window::platform::run_on_demand::EventLoopExtRunOnDemand;
+use rio_window::event_loop::{ActiveEventLoop, DeviceEvents};
+use rio_window::window::{Window, WindowId};
 use rio_window::{
-    dpi::LogicalSize,
-    event::{Event, WindowEvent},
-    event_loop::EventLoop,
-    window::WindowAttributes,
+    dpi::LogicalSize, event::WindowEvent, event_loop::EventLoop, window::WindowAttributes,
 };
-use sugarloaf::components::rect::Rect;
-use sugarloaf::layout::SugarloafLayout;
-use sugarloaf::{Object, Sugarloaf, SugarloafWindow, SugarloafWindowSize};
+use std::error::Error;
+use sugarloaf::{
+    layout::SugarloafLayout, Object, Rect, Sugarloaf, SugarloafWindow,
+    SugarloafWindowSize,
+};
 
 fn main() {
-    let mut event_loop = EventLoop::new().unwrap();
-    let width = 1200.0;
-    let height = 800.0;
+    let width = 800.0;
+    let height = 400.0;
+    let window_event_loop = rio_window::event_loop::EventLoop::new().unwrap();
+    let mut application = Application::new(&window_event_loop, width, height);
+    let _ = application.run(window_event_loop);
+}
 
-    let window_attribute = WindowAttributes::default()
-        .with_title("Rect example")
-        .with_inner_size(LogicalSize::new(width, height))
-        .with_resizable(true);
-    #[allow(deprecated)]
-    let window = event_loop.create_window(window_attribute).unwrap();
+struct Application {
+    sugarloaf: Option<Sugarloaf<'static>>,
+    window: Option<Window>,
+    height: f32,
+    width: f32,
+}
 
-    let scale_factor = window.scale_factor();
-    let font_size = 60.;
-    let line_height = 1.0;
+impl Application {
+    fn new(event_loop: &EventLoop<()>, width: f32, height: f32) -> Self {
+        event_loop.listen_device_events(DeviceEvents::Never);
 
-    let sugarloaf_layout = SugarloafLayout::new(
-        width as f32,
-        height as f32,
-        (0.0, 0.0, 0.0),
-        scale_factor as f32,
-        font_size,
-        line_height,
-    );
+        Application {
+            sugarloaf: None,
+            window: None,
+            width,
+            height,
+        }
+    }
 
-    let size = window.inner_size();
-    let sugarloaf_window = SugarloafWindow {
-        handle: window.window_handle().unwrap().into(),
-        display: window.display_handle().unwrap().into(),
-        scale: scale_factor as f32,
-        size: SugarloafWindowSize {
-            width: size.width as f32,
-            height: size.height as f32,
-        },
-    };
+    fn run(&mut self, event_loop: EventLoop<()>) -> Result<(), Box<dyn Error>> {
+        let result = event_loop.run_app(self);
+        result.map_err(Into::into)
+    }
+}
 
-    let font_library = sugarloaf::font::FontLibrary::default();
-    let mut sugarloaf = Sugarloaf::new(
-        sugarloaf_window,
-        sugarloaf::SugarloafRenderer::default(),
-        &font_library,
-        sugarloaf_layout,
-    )
-    .expect("Sugarloaf instance should be created");
+impl ApplicationHandler for Application {
+    fn resumed(&mut self, active_event_loop: &ActiveEventLoop) {
+        let window_attribute = WindowAttributes::default()
+            .with_title("Rect example")
+            .with_inner_size(LogicalSize::new(self.width, self.height))
+            .with_resizable(true);
+        let window = active_event_loop.create_window(window_attribute).unwrap();
 
-    #[allow(deprecated)]
-    let _ = event_loop.run_on_demand(move |event, event_loop_window_target| {
-        event_loop_window_target.set_control_flow(ControlFlow::Wait);
+        let scale_factor = window.scale_factor();
+        let font_size = 25.;
 
-        let mut objects = Vec::with_capacity(5);
-        objects.push(Object::Rect(Rect {
-            position: [10.0, 10.0],
-            color: [1.0, 1.0, 1.0, 1.0],
-            size: [1.0, 1.0],
-        }));
-        objects.push(Object::Rect(Rect {
-            position: [15.0, 10.0],
-            color: [1.0, 1.0, 1.0, 1.0],
-            size: [10.0, 10.0],
-        }));
-        objects.push(Object::Rect(Rect {
-            position: [30.0, 20.0],
-            color: [1.0, 1.0, 0.0, 1.0],
-            size: [50.0, 50.0],
-        }));
-        objects.push(Object::Rect(Rect {
-            position: [200., 200.0],
-            color: [0.0, 1.0, 0.0, 1.0],
-            size: [100.0, 100.0],
-        }));
-        objects.push(Object::Rect(Rect {
-            position: [500.0, 200.0],
-            color: [1.0, 1.0, 0.0, 1.0],
-            size: [200.0, 200.0],
-        }));
+        let sugarloaf_layout = SugarloafLayout::new(
+            self.width,
+            self.height,
+            (10.0, 10.0, 0.0),
+            scale_factor as f32,
+            font_size,
+            1.0,
+        );
+
+        let size = window.inner_size();
+        let sugarloaf_window = SugarloafWindow {
+            handle: window.window_handle().unwrap().into(),
+            display: window.display_handle().unwrap().into(),
+            scale: scale_factor as f32,
+            size: SugarloafWindowSize {
+                width: size.width as f32,
+                height: size.height as f32,
+            },
+        };
+
+        let sugarloaf = Sugarloaf::new(
+            sugarloaf_window,
+            sugarloaf::SugarloafRenderer::default(),
+            &sugarloaf::font::FontLibrary::default(),
+            sugarloaf_layout,
+        )
+        .expect("Sugarloaf instance should be created");
+
+        window.request_redraw();
+
+        self.sugarloaf = Some(sugarloaf);
+        self.window = Some(window);
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        if self.sugarloaf.is_none() || self.window.is_none() {
+            return;
+        }
+
+        let objects = vec![
+            Object::Rect(Rect {
+                position: [10.0, 10.0],
+                color: [1.0, 1.0, 1.0, 1.0],
+                size: [1.0, 1.0],
+            }),
+            Object::Rect(Rect {
+                position: [15.0, 10.0],
+                color: [1.0, 1.0, 1.0, 1.0],
+                size: [10.0, 10.0],
+            }),
+            Object::Rect(Rect {
+                position: [30.0, 20.0],
+                color: [1.0, 1.0, 0.0, 1.0],
+                size: [50.0, 50.0],
+            }),
+            Object::Rect(Rect {
+                position: [200., 200.0],
+                color: [0.0, 1.0, 0.0, 1.0],
+                size: [100.0, 100.0],
+            }),
+            Object::Rect(Rect {
+                position: [500.0, 200.0],
+                color: [1.0, 1.0, 0.0, 1.0],
+                size: [200.0, 200.0],
+            }),
+        ];
+
+        let sugarloaf = self.sugarloaf.as_mut().unwrap();
+        let window = self.window.as_mut().unwrap();
 
         match event {
-            Event::Resumed => {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::ScaleFactorChanged {
+                // mut inner_size_writer,
+                scale_factor,
+                ..
+            } => {
+                let scale_factor_f32 = scale_factor as f32;
+                let new_inner_size = window.inner_size();
+                sugarloaf.rescale(scale_factor_f32);
+                sugarloaf.resize(new_inner_size.width, new_inner_size.height);
                 window.request_redraw();
             }
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => event_loop_window_target.exit(),
-                WindowEvent::ScaleFactorChanged {
-                    inner_size_writer: _,
-                    scale_factor,
-                    ..
-                } => {
-                    let new_inner_size = window.inner_size();
-                    sugarloaf.rescale(scale_factor as f32);
-                    sugarloaf.resize(new_inner_size.width, new_inner_size.height);
-
-                    sugarloaf.set_objects(objects);
-                    sugarloaf.render();
-                }
-                WindowEvent::RedrawRequested { .. } => {
-                    sugarloaf.set_objects(objects);
-                    sugarloaf.render();
-                }
-                _ => (),
-            },
-            _ => {
-                event_loop_window_target.set_control_flow(ControlFlow::Wait);
+            WindowEvent::Resized(new_size) => {
+                sugarloaf.resize(new_size.width, new_size.height);
+                sugarloaf.set_objects(objects);
+                window.request_redraw();
             }
+            WindowEvent::RedrawRequested { .. } => {
+                sugarloaf.set_objects(objects);
+                sugarloaf.render();
+                event_loop.set_control_flow(ControlFlow::Wait);
+            }
+            _ => (),
         }
-    });
+    }
 }
