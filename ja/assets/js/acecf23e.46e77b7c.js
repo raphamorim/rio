576113eb1a8ce4a5a838e3dc18e79bffa7f96f64
raphamorim <raphamorim@users.noreply.github.com>
2024-09-21 21:15:16 +0000

    deploy: 665f68dc18f9441d19a2ef9796009f92090970e4

---
+++ ja/assets/js/acecf23e.46e77b7c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrio_docs=self.webpackChunkrio_docs||[]).push([[7393],{1838:o=>{o.exports=JSON.parse('{"blogBasePath":"/rio/ja/blog","blogTitle":"Blog","authorsListPath":"/rio/ja/blog/authors"}')}}]);
---
+++ ja/assets/js/b8716b19.b8f18be8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrio_docs=self.webpackChunkrio_docs||[]).push([[374],{5122:o=>{o.exports=JSON.parse('{"metadata":{"permalink":"/rio/ja/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":10,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
