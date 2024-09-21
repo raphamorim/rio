656679471ec53d858b2c614b3e795e1f500584ae
Raphael Amorim <rapha850@gmail.com>
2024-09-21 10:24:20 +0200

    increase macos fps to 240

--- rio-backend/src/config/defaults.rs
+++ rio-backend/src/config/defaults.rs
@@ -18,7 +18,7 @@ pub fn default_cursor_interval() -> u64 {
 #[inline]
 pub fn default_max_fps() -> u64 {
     if cfg!(target_os = "macos") {
-        180
+        240
     } else {
         90
     }
