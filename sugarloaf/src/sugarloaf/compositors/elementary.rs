8053cc5cdd34ce0c8d823ba67a4cbc5d3fef8c33
Raphael Amorim <rapha850@gmail.com>
2024-10-05 20:49:33 +0200

    start refactoring context

--- sugarloaf/src/sugarloaf/compositors/elementary.rs
+++ sugarloaf/src/sugarloaf/compositors/elementary.rs
@@ -3,8 +3,8 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-use crate::sugarloaf::Context;
 use crate::components::text::glyph::OwnedSection;
+use crate::sugarloaf::Context;
 use crate::sugarloaf::RootStyle;
 use crate::sugarloaf::{PxScale, Rect};
 use crate::{ComposedQuad, Text};
