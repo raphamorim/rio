19baea29075198f9baa188c493fd300fe58184ff
Tobias Hunger <tobias.hunger@gmail.com>
2024-11-13 19:09:51 +0100

    Unify handling of title updating
    ... and make sure to update the title straight away
    and not leave it unset for 2s.

--- frontends/rioterm/src/context/mod.rs
+++ frontends/rioterm/src/context/mod.rs
@@ -125,7 +125,7 @@ pub struct ContextManagerConfig {
 }
 
 pub struct ContextManagerTitles {
-    last_title_update: Instant,
+    last_title_update: Option<Instant>,
     pub titles: HashMap<usize, [String; 3]>,
     pub key: String,
 }
@@ -137,11 +137,10 @@ impl ContextManagerTitles {
         terminal_title: String,
         path: String,
     ) -> ContextManagerTitles {
-        let last_title_update = Instant::now();
         ContextManagerTitles {
             key: format!("{}{}{};", idx, program, terminal_title),
             titles: HashMap::from([(idx, [program, terminal_title, path])]),
-            last_title_update,
+            last_title_update: None,
         }
     }
 
@@ -642,11 +641,16 @@ impl<T: EventListener + Clone + std::marker::Send + 'static> ContextManager<T> {
             return;
         }
 
-        #[cfg(unix)]
+        let interval_time = Duration::from_secs(2);
+        if self
+            .titles
+            .last_title_update
+            .map(|i| i.elapsed() > interval_time)
+            .unwrap_or(true)
         {
-            let interval_time = Duration::from_secs(2);
-            if self.titles.last_title_update.elapsed() > interval_time {
-                self.titles.last_title_update = Instant::now();
+            self.titles.last_title_update = Some(Instant::now());
+            #[cfg(unix)]
+            {
                 let mut id = String::default();
                 for (i, context) in self.contexts.iter_mut().enumerate() {
                     let program = teletypewriter::foreground_process_name(
@@ -691,12 +695,9 @@ impl<T: EventListener + Clone + std::marker::Send + 'static> ContextManager<T> {
                 }
                 self.titles.set_key(id);
             }
-        }
 
-        #[cfg(not(unix))]
-        {
-            if self.titles.last_title_update.elapsed() > Duration::from_secs(2) {
-                self.titles.last_title_update = Instant::now();
+            #[cfg(not(unix))]
+            {
                 let mut id = String::from("");
                 for (i, _context) in self.contexts.iter().enumerate() {
                     let program = self.config.shell.program.to_owned();
