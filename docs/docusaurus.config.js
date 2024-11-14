63cebd788ad74cd0ddfedb40c840fac880a27cc1
Raphael Amorim <rapha850@gmail.com>
2024-11-14 13:57:51 +0100

    add gtag plugin

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -80,10 +80,10 @@ const config = {
             require.resolve('./src/css/custom.css'),
           ],
         },
-        // gtag: {
-        //   trackingID: '---------',
-        //   anonymizeIP: true,
-        // },
+        gtag: {
+          trackingID: 'G-6MKJ1X7CFS',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
