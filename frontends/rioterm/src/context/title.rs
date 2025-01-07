0b5988e71999e884d8b042711759cf20ec62c464
Raphael Amorim <rapha850@gmail.com>
2025-01-07 12:50:00 -0300

    add more tests

--- frontends/rioterm/src/context/title.rs
+++ frontends/rioterm/src/context/title.rs
@@ -1,23 +1,34 @@
 use crate::context::Context;
 
 #[inline]
-fn update_title<T: rio_backend::event::EventListener>(template: String, context: Context<T>) -> String {
-	let columns = "{{COLUMNS}}";
-	if template.contains(columns) {
-		let _ = template.replace(columns, "66");
-	}
-	template
+fn update_title<T: rio_backend::event::EventListener>(
+    template: String,
+    context: &Context<T>,
+) -> String {
+    let mut template = template.to_owned();
+
+    let columns = "{{COLUMNS}}";
+    if template.contains(columns) {
+        template = template.replace(columns, &context.dimension.columns.to_string());
+    }
+
+    let columns = "{{LINES}}";
+    if template.contains(columns) {
+        template = template.replace(columns, &context.dimension.columns.to_string());
+    }
+
+    template
 }
 
 #[cfg(test)]
 pub mod test {
-	use super::*;
-	use crate::context::ContextDimension;
-	use crate::context::Delta;
+    use super::*;
     use crate::context::create_mock_context;
+    use crate::context::ContextDimension;
+    use crate::context::Delta;
     use rio_backend::event::VoidListener;
-	use rio_window::window::WindowId;
-	use rio_backend::sugarloaf::layout::SugarDimensions;
+    use rio_backend::sugarloaf::layout::SugarDimensions;
+    use rio_window::window::WindowId;
 
     #[test]
     fn test_update_title() {
@@ -36,7 +47,7 @@ pub mod test {
         assert_eq!(context_dimension.columns, 66);
         assert_eq!(context_dimension.lines, 88);
 
-    	let rich_text_id = 0;
+        let rich_text_id = 0;
         let route_id = 0;
         let context = create_mock_context(
             VoidListener {},
@@ -45,15 +56,26 @@ pub mod test {
             rich_text_id,
             context_dimension,
         );
-    	assert_eq!(String::from(""), update_title(String::from(""), context));
-
-    	let context = create_mock_context(
-            VoidListener {},
-            WindowId::from(0),
-            route_id,
-            rich_text_id,
-            context_dimension,
+        assert_eq!(update_title(String::from(""), &context), String::from(""));
+        assert_eq!(
+            update_title(String::from("{{columns}}"), &context),
+            String::from("66")
         );
-    	assert_eq!(String::from("66"), update_title(String::from("{{COLUMNS}}"), context));
+        assert_eq!(
+            update_title(String::from("{{COLUMNS}}"), &context),
+            String::from("66")
+        );
+        assert_eq!(
+            update_title(String::from("{{ COLUMNS }}"), &context),
+            String::from("66")
+        );
+        assert_eq!(
+            update_title(String::from("{{ columns }}"), &context),
+            String::from("66")
+        );
+        assert_eq!(
+            update_title(String::from("hello {{ COLUMNS }} AbC"), &context),
+            String::from("hello 66 AbC")
+        );
     }
 }
