aebf5d01a761b6dc6af81ec27f211d572e105c51
Raphael Amorim <rapha850@gmail.com>
2025-02-25 18:37:30 -0300

    update description

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
   return (
     <Layout
       title={title}
-      description="Description will go into a meta tag in <head />"
+      description="A modern terminal for the 21st century."
     >
       <HomepageHeader />
       <main>
