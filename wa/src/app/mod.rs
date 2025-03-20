450a0be317eaeb0526afb9388f9c85ea06efb287
Raphael Amorim <rapha850@gmail.com>
2025-03-20 16:08:09 +0000

    remove wa

--- wa/src/app/mod.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(any(target_os = "macos", target_os = "ios"))]
-mod macos;
-
-#[cfg(any(target_os = "macos", target_os = "ios"))]
-pub use macos::*;
--- wa/src/resources/images/mod.rs
+++
@@ -1,1 +0,0 @@
-pub const LOGO_ICON: &[u8; 410598] = include_bytes!("./rio-logo.ico");
