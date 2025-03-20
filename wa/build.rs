b9ae9b29fedabbe8efa1c1937d14247aa030516e
Raphael Amorim <rapha850@gmail.com>
2025-03-20 16:08:09 +0000

    remove wa

--- wa/build.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-
-fn main() {
-    let target = env::var("TARGET").unwrap_or_else(|e| panic!("{}", e));
-
-    if target.contains("darwin") {
-        println!("cargo:rustc-link-lib=framework=MetalKit");
-    }
-}
