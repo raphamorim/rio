b38c50d23ac7ee2c7b0f5094451510268bf19fb2
Raphael Amorim <rapha850@gmail.com>
2025-04-09 10:32:51 +0200

    quadrants and shades

--- corcovado/src/timer.rs
+++ corcovado/src/timer.rs
@@ -175,10 +175,13 @@ impl<T> Timer<T> {
         let num_slots = num_slots.next_power_of_two();
         let capacity = capacity.next_power_of_two();
         let mask = (num_slots as u64) - 1;
-        let wheel = std::iter::repeat_n(WheelEntry {
-            next_tick: TICK_MAX,
-            head: EMPTY,
-        }, num_slots)
+        let wheel = std::iter::repeat_n(
+            WheelEntry {
+                next_tick: TICK_MAX,
+                head: EMPTY,
+            },
+            num_slots,
+        )
         .collect();
 
         Timer {
