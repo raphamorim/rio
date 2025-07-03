32ba0308cdcc18e8532bb26b56fb30edd1cabd0f
Raphael Amorim <rapha850@gmail.com>
2025-07-03 13:45:17 +0200

    bottomtab is now default for every os

--- rio-backend/src/config/navigation.rs
+++ rio-backend/src/config/navigation.rs
@@ -2,32 +2,19 @@ use crate::config::colors::{deserialize_to_arr, ColorArray};
 use crate::config::default_bool_true;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Clone, Copy)]
+#[derive(Default, Debug, Serialize, Deserialize, PartialEq, Clone, Copy)]
 pub enum NavigationMode {
     #[serde(alias = "plain")]
     Plain,
     #[serde(alias = "toptab")]
     TopTab,
-    #[cfg(target_os = "macos")]
     #[serde(alias = "nativetab")]
     NativeTab,
     #[serde(alias = "bottomtab")]
+    #[default]
     BottomTab,
     #[serde(alias = "bookmark")]
     Bookmark,
-}
-
-#[allow(clippy::derivable_impls)]
-impl Default for NavigationMode {
-    fn default() -> NavigationMode {
-        #[cfg(target_os = "macos")]
-        {
-            NavigationMode::NativeTab
-        }
-
-        #[cfg(not(target_os = "macos"))]
-        NavigationMode::Bookmark
-    }
 }
 
 impl NavigationMode {
