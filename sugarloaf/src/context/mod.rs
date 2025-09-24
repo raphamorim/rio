1dfb57ed098a5c921bb1a4be7514e991b183a537
Raphael Amorim <rapha850@gmail.com>
2025-09-24 12:58:55 +0200

    work in progress (quad ok)

--- sugarloaf/src/context/mod.rs
+++ sugarloaf/src/context/mod.rs
@@ -2,7 +2,7 @@ pub mod metal;
 pub mod webgpu;
 
 use crate::sugarloaf::{SugarloafBackend, SugarloafWindow};
-use crate::SugarloafRenderer;
+use crate::{SugarloafRenderer, SugarloafWindowSize};
 
 pub struct Context<'a> {
     pub inner: ContextType<'a>,
@@ -22,10 +22,46 @@ impl Context<'_> {
             SugarloafBackend::Wgpu(backends) => ContextType::Wgpu(
                 webgpu::WgpuContext::new(sugarloaf_window, renderer_config, backends),
             ),
-            SugarloafBackend::Metal => ContextType::Metal(metal::MetalContext::new()),
+            SugarloafBackend::Metal => ContextType::Metal(metal::MetalContext::new(sugarloaf_window)),
         };
 
         Context { inner }
+    }
+
+    #[inline]
+    pub fn scale(&self) -> f32 {
+        match &self.inner {
+            ContextType::Wgpu(ctx) => {
+                ctx.scale
+            }
+            ContextType::Metal(ctx) => {
+                ctx.scale
+            }
+        }
+    }
+
+    #[inline]
+    pub fn set_scale(&mut self, scale: f32) {
+        match &mut self.inner {
+            ContextType::Wgpu(ctx) => {
+                ctx.set_scale(scale);
+            }
+            ContextType::Metal(ctx) => {
+                ctx.set_scale(scale);
+            }
+        }
+    }
+
+    #[inline]
+    pub fn size(&self) -> SugarloafWindowSize {
+        match &self.inner {
+            ContextType::Wgpu(ctx) => {
+                ctx.size
+            }
+            ContextType::Metal(ctx) => {
+                ctx.size
+            }
+        }
     }
 
     pub fn resize(&mut self, width: u32, height: u32) {
