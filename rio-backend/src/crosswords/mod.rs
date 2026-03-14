375e651b1477a1db65550bc2224a81a014da2793
Raphael Amorim <rapha850@gmail.com>
2026-03-14 09:09:43 +0100

    oopsie

--- rio-backend/src/crosswords/mod.rs
+++ rio-backend/src/crosswords/mod.rs
@@ -3129,8 +3129,6 @@ impl<U: EventListener> Handler for Crosswords<U> {
                             offset_y as usize,
                             cell_width,
                             cell_height,
-                            width as usize,
-                            height as usize,
                         ) =>
                     {
                         // Ensure that we don't exceed the graphics limit per cell.
