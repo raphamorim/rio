53de1a644bfc55dd720ae6620099b6c196d250c0
Raphael Amorim <rapha850@gmail.com>
2026-03-29 13:51:10 +0200

    introduce effects

--- frontends/rioterm/src/application.rs
+++ frontends/rioterm/src/application.rs
@@ -162,7 +162,11 @@ impl ApplicationHandler<EventPayload> for Application<'_> {
             return;
         }
 
-        let theme = self.config.force_theme.map(|t| t.to_window_theme()).or(event_loop.system_theme());
+        let theme = self
+            .config
+            .force_theme
+            .map(|t| t.to_window_theme())
+            .or(event_loop.system_theme());
         update_colors_based_on_theme(&mut self.config, theme);
 
         self.router.create_window(
@@ -386,7 +390,11 @@ impl ApplicationHandler<EventPayload> for Application<'_> {
                     // Apply system theme to ensure colors are consistent
                     if !has_checked_adaptive_colors {
                         let system_theme = route.window.winit_window.theme();
-                        let theme = self.config.force_theme.map(|t| t.to_window_theme()).or(system_theme);
+                        let theme = self
+                            .config
+                            .force_theme
+                            .map(|t| t.to_window_theme())
+                            .or(system_theme);
                         update_colors_based_on_theme(&mut self.config, theme);
                         has_checked_adaptive_colors = true;
                     }
